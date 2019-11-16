89f81e19a8b5ce89473223e7f02d974c38c82687
TyPhyter <tylerjenkins08@gmail.com>
2019-11-15 22:30:38 -0600

    made image data object wider to cover the whole tongue hitbox, added some early returns on spotting of a red pixel, probably need to limit the check to two/three lines because there are some dropped frames

--- licker_bot.js
+++ licker_bot.js
@@ -4,15 +4,32 @@ const BUTTONS = {
 }
 
 const COLORS = {
-
+    BACKGROUND: "29,43,83,255",
+    RED: "255,0,77,255",
+    ORANGE: "255,163,0,255"
 }
 
 const e_canvas = document.querySelector("#e_canvas");
 const e_ctx = e_canvas.getContext("2d");
 
+let framesZPressed = 0;
+
 const getColors = () => {
 
-    const data = e_ctx.getImageData(64, 42, 1, 86).data;
+    if (pico8_buttons[0] === BUTTONS.Z) {
+
+        framesZPressed++;
+
+        if(framesZPressed === 2) {
+
+            framesZPressed = 0;
+
+            pico8_buttons[0] = 0;
+        }
+    }
+
+    // const data = e_ctx.getImageData(64, 42, 1, 86).data;
+    const data = e_ctx.getImageData(60, 42, 8, 86).data;
 
     const colors = [];
     let vals = [];
@@ -21,16 +38,28 @@ const getColors = () => {
     // try a slice version here too if this isn't performant
 
     for (let i = 0; i <= data.length; i++) {
-
+        // debugger;
         vals[counter] = data[i];
 
         if (counter === 3) {
-            colors.push(vals);
+
+            const color_str = vals.join(",");
+
+            if (color_str === COLORS.RED) return;
+
+            if (color_str !== COLORS.BACKGROUND) {
+
+                colors.push(color_str);
+            }
             counter = 0;
         }
         else {
             counter++;
         }
+    }
+
+    if (colors.includes(COLORS.ORANGE)) {
+        pico8_buttons[0] = BUTTONS.Z;
     }
 }
 
@@ -40,13 +69,9 @@ const oldRunner = Browser.mainLoop.runner;
 
 Browser.mainLoop.runner = () => {
     count++;
-    if (count === 59) {
+    if (count === 1) {
         count = 0;
         getColors();
-        pico8_buttons[0] = BUTTONS.Z;
-    }
-    else if (count === 29) {
-        pico8_buttons[0] = BUTTONS.X;
     }
 	oldRunner();
 }
